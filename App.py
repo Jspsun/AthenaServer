14e2b086af498f8240efca07d7e63e51b047af0e
Jspsun <Jspsun@gmail.com>
2017-06-02 12:56:30 -0400

    tweaked port

--- App.py
+++ App.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 # http://127.0.0.1:5000/
 
 
-@app.route('/', methods=['POST'])
+@app.route('/', methods=['GET'])
 def api_root():
 
     if request.headers['Content-Type'] == 'application/json':
@@ -16,5 +16,5 @@ def api_root():
 
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 1337))
     app.run(host='0.0.0.0', port=port)
---
+++ README.md
@@ -0,0 +1,4 @@
+# Athena Server
+Server that lets you post to it and will return a JSON.
+
+## Just using it as a tester server for now
