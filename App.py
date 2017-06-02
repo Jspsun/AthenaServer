303240b151e68c541ce4b00fa239e3ac77ab519f
Jspsun <Jspsun@gmail.com>
2017-06-01 23:35:42 -0400

    Initial build of flask server to accept JSON

--- App.py
+++ App.py
@@ -1,13 +1,20 @@
-from flask import Flask, url_for
+from flask import Flask, json, request
+import os
+
 app = Flask(__name__)
 
 # http://127.0.0.1:5000/
 
 
-@app.route('/<message>')
-def api_root(message):
-    return message
+@app.route('/', methods=['POST'])
+def api_root():
+
+    if request.headers['Content-Type'] == 'application/json':
+        return "JSON Message: " + json.dumps(request.json)
+    else:
+        return "Unsuported request. Please post a JSON"
 
 
 if __name__ == '__main__':
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
--- Tests/TesterClientStringQuery.py
+++ Tests/TesterClientStringQuery.py
@@ -1,4 +1,4 @@
 import requests
-res = urllib.urlopen('http://localhost:5000/api/add_message/1234')
+res = urllib.urlopen('http://localhost:5000/Amessage')
 if res.ok:
     print res.json()
