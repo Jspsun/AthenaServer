6edaa2f91d3295ca73142c7ece71ebe82aaa7695
Jspsun <Jspsun@gmail.com>
2017-06-04 00:40:47 -0400

    added some documentation to the app for the required JSON

--- App.py
+++ App.py
@@ -1,10 +1,17 @@
 from flask import Flask, json, request
+from CommandHandler import CommandCenter
 import os
 import Config
 
 app = Flask(__name__)
 
-# http://127.0.0.1:5000/
+# Post a json to flask server
+# Requires:
+#   {
+#   Authentication: INSERT AUTH CODE
+#   ClientType: TYPE OF DEVICE
+#   Message : CONTENT TO PARSE
+#   }
 
 
 @app.route('/', methods=['Post'])
@@ -16,12 +23,15 @@ def api_root():
     data = json.loads(json.dumps(request.json))
 
     # validate that JSON has all the needed parameters (Message)
-    if not('Authenication' in data and "Message" in data):
+    if not('Authenication' in data and "ClientType" in data and "Message" in data):
         return "Please send a JSON with valid data"
 
     # validate that authenication token is correct
     if data['Authenication'] != Config.Authentication:
         return "Incorrect authentication"
+
+    # Pass data to command center
+    CommandCenter.process(data['Message'])
 
     return "Hi " + Config.Authentication
 
