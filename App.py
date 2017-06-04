33fa0cdad61192199a983e1307e2627b3df12158
Jspsun <Jspsun@gmail.com>
2017-06-04 00:14:57 -0400

    Added input sanitization

--- App.py
+++ App.py
@@ -1,20 +1,31 @@
 from flask import Flask, json, request
 import os
+import Config
 
 app = Flask(__name__)
 
 # http://127.0.0.1:5000/
 
 
-@app.route('/', methods=['GET'])
+@app.route('/', methods=['Post'])
 def api_root():
+    # validate that user sends in a json
+    if request.headers['Content-Type'] != 'application/json':
+        return "Please post a JSON"
 
-    if request.headers['Content-Type'] == 'application/json':
-        return "JSON Message: " + json.dumps(request.json)
-    else:
-        return "Unsuported request. Please post a JSON"
+    data = json.loads(json.dumps(request.json))
+
+    # validate that JSON has all the needed parameters (Message)
+    if not('Authenication' in data and "Message" in data):
+        return "Please send a JSON with valid data"
+
+    # validate that authenication token is correct
+    if data['Authenication'] != Config.Authentication:
+        return "Incorrect authentication"
+
+    return "Hi " + Config.Authentication
 
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 1337))
-    app.run(host='0.0.0.0', port=port)
+    app.run(debug=True, host='localhost', port=port)
